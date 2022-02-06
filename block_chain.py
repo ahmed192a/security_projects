6d4d988d11ecfaeb52b848abb73e6a391192c42f
ahmed mohamed <42156372+ahmed192a@users.noreply.github.com>
2022-02-06 23:07:06 +0200

    upload first edition of security project

---
+++ block_chain.py
@@ -0,0 +1,125 @@
+from hashlib import sha256
+import json
+import time
+
+from flask import Flask, request
+import requests
+
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = 0
+
+    def compute_hash(self):
+        """
+        A function that return the hash of the block contents.
+        """
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+    # difficulty of our PoW algorithm
+    difficulty = 2
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        """
+        A function to generate genesis block and appends it to
+        the chain. The block has index 0, previous_hash as 0, and
+        a valid hash.
+        """
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def add_block(self, block):
+        """
+        A function that adds the block to the chain 
+        """
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def proof_of_work(self, block):
+        """
+        Function that tries different values of nonce to get a hash
+        that satisfies our difficulty criteria.
+        """
+        block.nonce = 0
+
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+
+        block.hash = computed_hash
+        return computed_hash
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        """
+        This function serves as an interface to add the pending
+        transactions to the blockchain by adding them to the block
+        and figuring out Proof Of Work.
+        """
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
+                          previous_hash=last_block.hash)
+
+        self.proof_of_work(new_block)
+        self.add_block(new_block)
+
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    tran = request.args.get('tran')
+    blockchain.add_new_transaction(tran)
+    blockchain.mine()
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+
+
+
+
+
+app.run(debug=True, port=5000)
---
+++ usb_scanner.py
@@ -0,0 +1,58 @@
+from pyudev import Context, Monitor
+
+#import pyudev
+import psutil
+import glob
+import hashlib
+import time
+
+
+
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
+
+virus_hash = '68bb75d24bdb38da03227dc3d90452019f96c5c1'
+context = Context()
+monitor = Monitor.from_netlink(context)
+monitor.filter_by(subsystem='block')
+
+i = 0
+
+while(1):
+    for action, device in monitor:
+        #print(i)
+        i=(i+1)%2
+        if(action == 'add' and i == 0):
+            print('{0}: {1}'.format(action, device.device_node))
+            partition = device.device_node
+            time.sleep(1)
+            for p in psutil.disk_partitions():
+                #print(p.device)
+                if p.device == partition:
+                    print("  {}: {}".format(p.device, p.mountpoint))
+                    files = glob.glob(p.mountpoint + "/*.txt")
+                    print("***list of files:\n")
+                    for f in files:
+                        print("\t\t"+f)
+                        if(virus_hash == hash_file(f) ):
+                            print("\n************** x USB has VIRUS x **************")
+                            break;
+
+            
